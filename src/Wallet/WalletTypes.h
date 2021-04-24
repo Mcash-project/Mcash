42de531f2b8f2e3bbddb665da7aaf54ea55ea6ad
yourlitdaddy <mateo200525@gmail.com>
2021-04-23 17:37:23 -0700

    update

---
+++ src/Wallet/WalletTypes.h
@@ -0,0 +1,20 @@
+
+#pragma once
+
+#include <CryptoNote.h>
+#include <optional>
+#include <string>
+#include <unordered_map>
+#include <numeric>
+
+#include "Serialization/ISerializer.h"
+#include "Serialization/SerializationOverloads.h"
+#include "CryptoNoteCore/CryptoNoteSerialization.h"
+
+namespace WalletTypes {
+
+
+    /* A 'block' with the very basics needed to sync the transactions */
+
+
+}
